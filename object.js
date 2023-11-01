f04bb1509c84f6df83a381fad347fc28aec0f8b0
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-11-01 23:49:56 +0530

    added

--- object.js
+++ object.js
@@ -3,9 +3,19 @@ const empty = { };
 console.log(empty);
 
 //  02 Write a JavaScript program to create an object with properties and methods.Create an object called "person" with properties "name" and "age." and also create a function into that object call IntrouduceYou take a person object as a parameter and logs a massage to the console.
-let person = {
-    name: 'John',
-    age: 30
+// let person = {
+//     name: 'Piratheep',
+//     age: 21,
     
-    };
+//     introduceOfYou: function() {
+//         console.log("Hello, My name is "+this.name);
+//     }
+        
+//     };
+//     person.introduceOfYou();
+
+
+    //  03 
+
+    
 
