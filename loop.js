3a59f24198504019ba4a6e632bfd75adf4feb1a6
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-11-06 22:18:10 +0530

    added

---
+++ loop.js
@@ -0,0 +1,32 @@
+//  01
+
+for (let i = 0; i < 5; i++) {
+    
+   console.log('Hi John!');
+    
+}
+
+
+//  02
+let i = 0;
+while (i <= 10) {
+
+    console.log(i);
+
+    i++;
+}
+
+//  03
+let x = 1;
+while (x <= 10) {
+
+    console.log(x**2);
+
+    x++;
+}
+
+// 04
+for (let m = 0; m <= 100; m+=2) {
+    console.log(m);
+    
+}
--- switch.js
+++ switch.js
@@ -1,35 +1,37 @@
-let month = prompt('Enter a month number: ')
-let year = prompt('Enter the year: ')
+// let month = parseInt(prompt('Enter a month number: '));
+// let year = 2023;
 
-switch (month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
+// switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
 
-        console.log('The number of days in a month is 31.');
-        break;
+//         console.log('The number of days in a month is 31.');
+//         break;
 
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        console.log('The number of days in that month is 30.');
-        break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log('The number of days in that month is 30.');
+//         break;
     
-    case 2:
-        if (year % 4===0 ) {
-            console.log('The number of days is 29.');
-        }
+//     case 2:
+//         if (year % 4===0) {
+//             console.log('The number of days is 29.');
+//         }
 
-        else {
-            console.log('The number of days is 28.');
-        }
-        break;
+//         else {
+//             console.log('The number of days is 28.');
+//         }
+//         break;
+    
+//     case month>12:
+//         console.log('Invalid month.');
 
-    default:
-        console.log('Invalid input!');
-}
+// }
+
